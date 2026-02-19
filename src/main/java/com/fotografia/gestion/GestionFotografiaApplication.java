68a9b396f17e6f24f24ae332ce3f6e07cd4cc4ad
Luis Emilio Ruiz Vega <luemiruve@gmail.com>
2026-02-18 22:45:22 -0600

    Proyecto base Spring Boot inicial

---
+++ src/main/java/com/fotografia/gestion/GestionFotografiaApplication.java
@@ -0,0 +1,13 @@
+package com.fotografia.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFotografiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFotografiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fotografia/gestion/GestionFotografiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fotografia.gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFotografiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
